abda74e7121660160a8f2dc23dcc719126dabcae
Sarang7457 <82943672+Sarang7457@users.noreply.github.com>
2021-04-21 17:04:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios"; 
+
+function App() {
+  
+  const [search, setSerach] = useState("chiken");
+  const [recipes, setRecipes] = useState([]);
+
+  const APP_ID = "a8d25a5f";
+  const APP_KEY = "ee8417f418f940e0f40a35c9debbe7aa";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const res = await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    setRecipes(res.data.hits);
+};
+
+  const onInputChange = e => {
+    setSerach(e.target.value);
+  };
+
+  const onSearchClick = () => {
+    getRecipes();
+  };
+  return (
+    <div className="App">
+      <Header 
+      search={search} 
+      onInputChange={onInputChange}
+      onSearchClick={onSearchClick}
+       />
+      <div className="container">
+        <Recipes recipes={recipes} />
+        </div>
+    </div>
+  );
+}
+
+export default App;
